0daf752c5d8a58de2cc1f6723209ec33613f2947
lucky0523 <lucky0523@gmail.com>
2022-08-23 00:16:39 +0800

    确定主页风格

--- PhotoGallery/views.py
+++ PhotoGallery/views.py
@@ -11,3 +11,7 @@ from PhotoGallery.common import Static
 LOG_TAG = '[PhotoGallery.views] '
 logging.basicConfig(level=Static.LOG_LEVEL, format='%(asctime)s - %(name)s %(levelname)s - %(message)s')
 logger = logging.getLogger(LOG_TAG)
+
+
+def nav(request):
+    return render(request, 'navigation.html')
